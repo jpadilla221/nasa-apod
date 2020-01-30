d18d1ebcf514b10e0c3e39ea15bfb88ff0a6b2f5
Jonathan Padilla <jpadilla8104@gmail.com>
2020-01-30 14:00:05 -0700

    Date picking.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.webkit.ServiceWorkerWebSettings;
 import android.webkit.WebResourceRequest;
@@ -21,17 +22,22 @@ import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.android.DateTimePickerFragment;
+import edu.cnm.deepdive.android.DateTimePickerFragment.Mode;
+import edu.cnm.deepdive.android.DateTimePickerFragment.OnChangeListener;
 import edu.cnm.deepdive.nasaapod.BuildConfig;
 import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.model.Apod;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
 import edu.cnm.deepdive.nasaapod.viewmodel.MainViewModel;
 import java.io.IOException;
+import java.util.Calendar;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ImageFragment extends Fragment {
+
   private static final String IMAGE_URL =
       "https://apod.nasa.gov/apod/image/2001/ic410_WISEantonucci_1824.jpg";
 
@@ -48,6 +54,7 @@ public class ImageFragment extends Fragment {
     loading = root.findViewById(R.id.loading);
     calendar = root.findViewById(R.id.calendar);
     setupWebView(root);
+    setupCalendarPicker(Calendar.getInstance());
 
     return root;
   }
@@ -57,7 +64,12 @@ public class ImageFragment extends Fragment {
     super.onViewCreated(view, savedInstanceState);
     viewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
     viewModel.getApod().observe(getViewLifecycleOwner(),
-        (apod) -> contentView.loadUrl(apod.getUrl()));
+        (apod) -> {
+          contentView.loadUrl(apod.getUrl());
+          Calendar calendar = Calendar.getInstance();
+          calendar.setTime(apod.getDate());
+          setupCalendarPicker(calendar);
+        });
   }
 
   private void setupWebView(View root) {
@@ -82,6 +94,19 @@ public class ImageFragment extends Fragment {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
-      }
+  }
 
+  private void setupCalendarPicker(Calendar calendar) {
+    this.calendar.setOnClickListener(v -> {
+      DateTimePickerFragment fragment = new DateTimePickerFragment();
+      fragment.setCalendar(calendar);
+      fragment.setMode(Mode.DATE);
+      fragment.setOnChangeListener(calendar1 -> {
+        loading.setVisibility(View.VISIBLE);
+        viewModel.setApodDate(calendar1.getTime());
+      });
+      fragment.show(getChildFragmentManager(), fragment.getClass().getName());
+    });
+
+  }
 }
