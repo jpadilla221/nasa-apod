dd2e5ff96e12c97514f74da676c212436b0de8d6
Jonathan Padilla <jpadilla8104@gmail.com>
2020-01-31 09:51:20 -0700

    Added info alert.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
@@ -1,14 +1,13 @@
 package edu.cnm.deepdive.nasaapod.controller;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.webkit.ServiceWorkerWebSettings;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -18,30 +17,16 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.android.DateTimePickerFragment;
 import edu.cnm.deepdive.android.DateTimePickerFragment.Mode;
-import edu.cnm.deepdive.android.DateTimePickerFragment.OnChangeListener;
-import edu.cnm.deepdive.nasaapod.BuildConfig;
 import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.model.Apod;
-import edu.cnm.deepdive.nasaapod.service.ApodService;
 import edu.cnm.deepdive.nasaapod.viewmodel.MainViewModel;
-import java.io.IOException;
 import java.util.Calendar;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ImageFragment extends Fragment {
-
-  private static final String IMAGE_URL =
-      "https://apod.nasa.gov/apod/image/2001/ic410_WISEantonucci_1824.jpg";
 
   private WebView contentView;
   private MainViewModel viewModel;
@@ -50,7 +35,13 @@ public class ImageFragment extends Fragment {
   private Apod apod;
 
   @Override
-  @SuppressLint("SetJavaScriptEnabled")
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
+    setRetainInstance(true);
+  }
+
+  @Override
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_image, container, false);
@@ -58,7 +49,6 @@ public class ImageFragment extends Fragment {
     calendar = root.findViewById(R.id.calendar);
     setupWebView(root);
     setupCalendarPicker(Calendar.getInstance());
-
     return root;
   }
 
@@ -75,11 +65,41 @@ public class ImageFragment extends Fragment {
           this.apod = apod;
         });
     viewModel.getThrowable().observe(getViewLifecycleOwner(), (throwable) -> {
-      loading.setVisibility(view.GONE);
+      loading.setVisibility(View.GONE);
       Toast toast = Toast.makeText(getActivity(),
           getString(R.string.error_message, throwable.getMessage()), Toast.LENGTH_LONG);
-      toast.setGravity(Gravity.BOTTOM, 0,(int) getResources().getDimension(R.dimen.toast_vertical_margin));
+      toast.setGravity(Gravity.BOTTOM, 0,
+          (int) getResources().getDimension(R.dimen.toast_vertical_margin));
+      toast.show();
     });
+  }
+
+  @Override
+  public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+    super.onCreateOptionsMenu(menu, inflater);
+    inflater.inflate(R.menu.options, menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.info:
+        if (apod != null) {
+          InfoFragment fragment = new InfoFragment();
+          Bundle args = new Bundle();
+          args.putString(InfoFragment.TITLE_KEY, apod.getTitle());
+          args.putString(InfoFragment.DESCRIPTION_KEY, apod.getDescription());
+          args.putString(InfoFragment.COPYRIGHT_KEY, apod.getCopyright());
+          args.putSerializable(InfoFragment.DATE_KEY, apod.getDate());
+          fragment.setArguments(args);
+          fragment.show(getChildFragmentManager(), fragment.getClass().getName());
+        }
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
   }
 
   private void setupWebView(View root) {
@@ -88,15 +108,15 @@ public class ImageFragment extends Fragment {
       @Override
       public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
         return false;
-
       }
 
       @Override
       public void onPageFinished(WebView view, String url) {
-
         loading.setVisibility(View.GONE);
         Toast toast = Toast.makeText(getActivity(), apod.getTitle(), Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.BOTTOM, 0, (int) getContext().getResources().getDimension(R.dimen.toast_vertical_margin));
+        toast.setGravity(Gravity.BOTTOM, 0, (int) getContext().getResources()
+            .getDimensionPixelOffset(R.dimen.toast_vertical_margin));
+        toast.show();
       }
     });
     WebSettings settings = contentView.getSettings();
@@ -109,16 +129,16 @@ public class ImageFragment extends Fragment {
   }
 
   private void setupCalendarPicker(Calendar calendar) {
-    this.calendar.setOnClickListener(v -> {
+    this.calendar.setOnClickListener((v) -> {
       DateTimePickerFragment fragment = new DateTimePickerFragment();
       fragment.setCalendar(calendar);
       fragment.setMode(Mode.DATE);
-      fragment.setOnChangeListener(calendar1 -> {
+      fragment.setOnChangeListener((cal) -> {
         loading.setVisibility(View.VISIBLE);
-        viewModel.setApodDate(calendar1.getTime());
+        viewModel.setApodDate(cal.getTime());
       });
       fragment.show(getChildFragmentManager(), fragment.getClass().getName());
     });
-
   }
+
 }
