64d4f5e895831eb09f2ba96415c9631d0a8d9be8
Jonathan Padilla <jpadilla8104@gmail.com>
2020-02-04 16:25:40 -0700

    Added access entity and dao.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/viewmodel/MainViewModel.java
@@ -1,40 +1,26 @@
 package edu.cnm.deepdive.nasaapod.viewmodel;
 
-import static android.icu.text.Collator.getInstance;
-
-import android.annotation.SuppressLint;
 import android.app.Application;
-import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.nasaapod.BuildConfig;
-
 import edu.cnm.deepdive.nasaapod.model.dao.ApodDao;
 import edu.cnm.deepdive.nasaapod.model.entity.Apod;
 import edu.cnm.deepdive.nasaapod.service.ApodDatabase;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
-import io.reactivex.Scheduler;
 import io.reactivex.schedulers.Schedulers;
-import java.io.IOException;
 import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainViewModel extends AndroidViewModel {
 
-  private Date apodDate;
-  private MutableLiveData<Apod> apod;
-  private MutableLiveData<Throwable> throwable;
   private ApodDatabase database;
   private ApodService nasa;
+  private MutableLiveData<Apod> apod;
+  private MutableLiveData<Throwable> throwable;
 
   public MainViewModel(@NonNull Application application) {
     super(application);
@@ -42,13 +28,14 @@ public class MainViewModel extends AndroidViewModel {
     nasa = ApodService.getInstance();
     apod = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
-    Date today = new Date();
-    String formattedDate = ApodService.DATE_FORMATTER.format(today);
-    try {
-      setApodDate(ApodService.DATE_FORMATTER.parse(formattedDate)); // TODO Investigate adjustment for NASA APOD-relevant time zone.
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
-    }
+    Calendar today = Calendar.getInstance();
+    int offset = today.get(Calendar.ZONE_OFFSET);
+    today.set(Calendar.HOUR, 0);
+    today.set(Calendar.MINUTE, 0);
+    today.set(Calendar.SECOND, 0);
+    today.set(Calendar.MILLISECOND, 0);
+    today.set(Calendar.ZONE_OFFSET, offset);
+    setApodDate(today.getTime()); // TODO Investigate adjustment for NASA APOD-relevant time zone.
   }
 
   public LiveData<Apod> getApod() {
@@ -61,28 +48,26 @@ public class MainViewModel extends AndroidViewModel {
 
   public void setApodDate(Date date) {
     ApodDao dao = database.getApodDao();
+    DateFormat formatter = ApodService.DATE_FORMATTER;
     dao.select(date)
         .subscribeOn(Schedulers.io())
         .subscribe(
             (apod) -> this.apod.postValue(apod),
             (throwable) -> this.throwable.postValue(throwable),
-            () -> nasa.get(BuildConfig.API_KEY, ApodService.DATE_FORMATTER.format(date))
+            () -> nasa.get(BuildConfig.API_KEY, formatter.format(date))
                 .subscribeOn(Schedulers.io())
                 .subscribe(
                     (apod) -> dao.insert(apod)
                         .subscribeOn(Schedulers.io())
                         .subscribe(
-                            (id) ->{
+                            (id) -> {
                               apod.setId(id);
                               this.apod.postValue(apod);
                             },
                             (throwable) -> this.throwable.postValue(throwable)
                         ),
-                    (throwable) -> this.throwable.postValue(throwable)
-                )
+                    (throwable) -> this.throwable.postValue(throwable))
         );
-
   }
-
 
 }
